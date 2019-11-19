f460eb8c8ac5e27f3d13ed6396f3c3952c164ba9
pcysl5edgo <pcysl5edgo@gmail.com>
2019-11-19 23:38:48 +0900

    [update]sxml

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core = __importStar(require("@actions/core"));
+var sxml_1 = require("sxml");
 var os = __importStar(require("os"));
 var path = __importStar(require("path"));
 var fs = __importStar(require("fs"));
@@ -19,13 +20,23 @@ function getAbsolutePath(inputFilePath) {
         return path.join(homeDirectory, inputFilePath.slice(1));
     throw new Error("Unable to resole `~` to HOME");
 }
+function SetOutputFromProperty(node, item) {
+    core.setOutput(item, node.getProperty(item));
+}
 function Run() {
     var path = getAbsolutePath(core.getInput("path", { required: true }));
     try {
-        core.debug("Path : " + path);
         var text = fs.readFileSync(path, { encoding: "utf8" });
-        core.debug("Text : " + text);
-        core.setOutput("text", text);
+        var resultXml = new sxml_1.XML(text);
+        var root = resultXml.get("test-run").at(0);
+        SetOutputFromProperty(root, "testcasecount");
+        SetOutputFromProperty(root, "total");
+        SetOutputFromProperty(root, "passed");
+        SetOutputFromProperty(root, "failed");
+        SetOutputFromProperty(root, "inconclusive");
+        SetOutputFromProperty(root, "skipped");
+        var success = !root.getProperty("result").startsWith("Failed");
+        core.setOutput("success", success.toString());
     }
     catch (error) {
         core.setFailed(error.message);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
-import * as parser from 'fast-xml-parser';
-
+import * as xml from 'sxml';
+import { XML } from 'sxml';
 import * as os from "os";
 import * as path from "path";
 import * as fs from "fs";
@@ -16,13 +16,24 @@ function getAbsolutePath(inputFilePath: string): string {
     throw new Error("Unable to resole `~` to HOME");
 }
 
+function SetOutputFromProperty(node: XML, item: string) {
+    core.setOutput(item, node.getProperty(item));
+}
+
 function Run() {
     const path = getAbsolutePath(core.getInput("path", { required: true }));
     try {
-        core.debug("Path : " + path);
         const text = fs.readFileSync(path, { encoding: "utf8" });
-        core.debug("Text : " + text);
-        core.setOutput("text", text);
+        const resultXml = new XML(text);
+        const root = resultXml.get("test-run").at(0);
+        SetOutputFromProperty(root, "testcasecount");
+        SetOutputFromProperty(root, "total");
+        SetOutputFromProperty(root, "passed");
+        SetOutputFromProperty(root, "failed");
+        SetOutputFromProperty(root, "inconclusive");
+        SetOutputFromProperty(root, "skipped");
+        const success: Boolean = !root.getProperty("result").startsWith("Failed");
+        core.setOutput("success", success.toString());
     } catch (error) {
         core.setFailed(error.message);
     }
